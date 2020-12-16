4ee0e6af69641aacfd92b741f633b5686988cae7
gchuinard <gautier.chuinard@lilo.org>
2020-12-16 21:56:12 +0000

    add task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+for n1 in range(10):
+    for n2 in range(n1 + 1, 10):
+        if n1 < 8:
+            print("{}{}, ".format(n1, n2), end="")
+        else:
+            print("{}{}".format(n1, n2))
