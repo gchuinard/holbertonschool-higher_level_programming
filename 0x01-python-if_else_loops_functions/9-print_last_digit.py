f3d126880b21ba7c09ba8f2a606bb8fc143c4eb7
gchuinard <gautier.chuinard@lilo.org>
2020-12-17 16:54:11 +0000

    add task 10

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number > 9 or number < -9:
-        print("{:d}".format(number % 10 if number > 0 else -number % 10))
+        print("{:d}".format(number % 10 if number > 0 else -number % 10), end="")
     else:
-        print("{:d}".format(number if number >= 0 else - number))
+        print("{:d}".format(number if number >= 0 else - number), end="")
+    print_last_digit(98)
