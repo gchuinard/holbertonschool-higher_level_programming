f6b3605f5236e23678c42b246cefa68039f82d41
gchuinard <gautier.chuinard@lilo.org>
2021-01-09 15:55:49 +0000

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_arg():
+    
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+if __name__ == "__main__":
+    print_arg()
