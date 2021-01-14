8ac9881636b15eb548820ea172bd471f81a65fc7
gchuinard <gautier.chuinard@lilo.org>
2021-01-14 16:03:37 +0000

    add task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        n = 0
+        n = my_list[0]
         for nbr in my_list:
             if n < nbr:
                 n = nbr
