e64062521aa055d51ae0e18a10d39c87af8d47db
gchuinard <gautier.chuinard@lilo.org>
2021-01-21 22:47:11 +0000

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is class module: Square
+"""
+
+
+class Square:
+    """
+    Define a square
+    """
+
+    def __init__(self, size):
+        """
+        The constructor
+        args:
+            size (int): square size
+        """
+
+        self.__size = size
