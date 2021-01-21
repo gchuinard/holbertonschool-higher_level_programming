376ee098797d74509e581370274ec31601acf0fd
gchuinard <gautier.chuinard@lilo.org>
2021-01-21 22:58:57 +0000

    add task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is class module: Square
+"""
+
+
+class Square:
+    """
+    Define a square
+    """
+
+    def __init__(self, size = 0):
+        """
+        The constructor
+        args:
+            size (int): square size
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
