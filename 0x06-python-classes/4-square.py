7b9ed853b91a26c61e03c7ff0de0e49455919f78
gchuinard <gautier.chuinard@lilo.org>
2021-01-21 23:36:30 +0000

    add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is class module: Square
+"""
+
+
+class Square:
+    """
+    Define a square
+    """
+
+    def __init__(self, size=0):
+        """
+        The constructor
+        args:
+            size (int): square size
+        """
+        self.size = size 
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
