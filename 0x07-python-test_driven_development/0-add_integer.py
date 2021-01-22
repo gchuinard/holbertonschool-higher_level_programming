a962af00a96444bce332bb520f8e2580400e8443
gchuinard <gautier.chuinard@lilo.org>
2021-01-22 17:34:19 +0000

    add task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+add_integer - function that adds 2 integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    args:
+        a (int, float): the first integer.
+        b (int, float): the second integer.
+    return: the sun of a + b in integer or raise error if fail.
+    """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
