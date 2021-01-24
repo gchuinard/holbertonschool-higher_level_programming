6cee92b63f694de9a9ce8ae4dc126c00728f9c0f
gchuinard <gautier.chuinard@lilo.org>
2021-01-24 22:10:13 +0000

    add task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,7 +11,7 @@ def say_my_name(first_name, last_name=""):
         last_name (string): the last name.
     """
 
-    if not isinstance(first_name, str): 
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
