ebe9c8823400f7dcc21d11eaf1c11be2401def29
gchuinard <gautier.chuinard@lilo.org>
2021-01-24 21:25:15 +0000

    add task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+matrix_divided - function that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    args:
+        matrix (int, float): the first integer.
+        div (int, float): the second integer.
+    return: the a matrix with all the numbers divide by div or raise error if fail.
+    """
+
+    Error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        if not isinstance(i, list):
+            raise TypeError(Error_msg)
+        if len(matrix[0]) != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+    for tab in matrix:
+        for nbr in tab:
+            if not isinstance(nbr, int) and not isinstance(nbr, float):
+                raise TypeError(Error_msg)
+
+    return [[round(nbr / div, 2) for nbr in tab] for tab in matrix]
