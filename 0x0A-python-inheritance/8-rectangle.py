7fcf95ccc5d07887e6ab0444e3f221fe500b86ee
gchuinard <gautier.chuinard@lilo.org>
2021-01-27 19:24:39 +0000

    add task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,13 +3,18 @@
 class Rectangle
 """
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-class rectangle(BaseGeomerty):
+
+class Rectangle(BaseGeomerty):
     """
     inherits from BaseGeometry
     """
 
     def __init__(self, width, height):
+        """
+        constructor of Rectangle
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
