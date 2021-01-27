0b3308ac0ce39264af25741cf2f0b81e4b6cc3f4
gchuinard <gautier.chuinard@lilo.org>
2021-01-27 17:57:51 +0000

    add task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,11 +4,11 @@ class MyList
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     returns True if the object is an instance of, or if the object
     is an instance of a class that inherited from, the specified class;
     otherwise False.
     """
 
-    return isinstance(obj) is a_class
+    return isinstance(obj, a_class)
