90cd752ddd29b45b2c2586f6257c21822545968b
gchuinard <gautier.chuinard@lilo.org>
2021-01-27 18:25:44 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    class do nothing
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
