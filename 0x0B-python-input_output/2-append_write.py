7d89db4ddca5c1de9b381db2490471680cd7fe31
gchuinard <gautier.chuinard@lilo.org>
2021-01-28 03:36:10 +0000

    add task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+open a file in Python
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
