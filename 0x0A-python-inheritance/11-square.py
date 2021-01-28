32fdcd05acb665538baba861f0c18af77a9c2e73
gchuinard <gautier.chuinard@lilo.org>
2021-01-28 01:59:08 +0000

    add task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,19 +15,6 @@ class Square(Rectangle):
         """
         constructor of Square
         """
-        self.integer_validator("size", size)
+
+        super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """
-        return the description of the square
-        """
-
-        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
-
-    def area(self):
-        """
-        return the area of the square
-        """
-
-        return self.__size ** 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(BaseGeometry):
         return the description of the rectangle
         """
 
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        return "[{}] {:d}/{:d}".format(type(self).__name__, self.__width, self.__height)
 
     def area(self):
         """
