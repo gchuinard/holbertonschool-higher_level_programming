4685679c52961f2239e20bd811389c7e988c1077
gchuinard <gautier.chuinard@lilo.org>
2021-01-28 20:46:43 +0000

    add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student():
+    """
+    define a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        the constructor of Student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if attrs == None:
+            return self.__dict__
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
