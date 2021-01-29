d201b733b9d048cd3103bcc0c3d4f1b05a73682b
gchuinard <gautier.chuinard@lilo.org>
2021-01-29 19:18:11 +0000

    add task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+class Square
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    The class Square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        constructor of Rectangle
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        return the description of square in string
+        """
+
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
