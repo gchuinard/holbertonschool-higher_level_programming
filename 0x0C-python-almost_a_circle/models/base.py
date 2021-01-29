b5c976cfecdcb6a9c2e4eb52d630e93a33e59590
gchuinard <gautier.chuinard@lilo.org>
2021-01-29 22:41:01 +0000

    add task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,17 @@ class Base():
         if list_dictionaries is None or list_dictionaries == "":
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+        
+        new_list = []
+        if list_objs is not None:
+            for i in list_objs:
+                new_list.append(i.to_dictionary())
+
+        with open(cls.__name__ + ".json", 'w+', encoding="utf-8") as f:
+            f.write(cls.to_json_string(new_list))
