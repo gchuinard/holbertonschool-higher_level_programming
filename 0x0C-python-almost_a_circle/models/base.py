d5e560538df9e67af94bb86b2a2c5731d439a61f
gchuinard <gautier.chuinard@lilo.org>
2021-01-29 22:47:21 +0000

    add task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,13 @@ class Base():
 
         with open(cls.__name__ + ".json", 'w+', encoding="utf-8") as f:
             f.write(cls.to_json_string(new_list))
+
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+        return 
