453a546e66fc0c2ca41a11c844e9a49b3eb8b3b5
gchuinard <gautier.chuinard@lilo.org>
2021-03-03 16:36:45 +0000

    add task 11 - 16

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,7 @@
 
 SELECT ts.title, tsg.genre_id
 FROM `tv_shows` ts
-LEFT JOIN `tv_show_genres` tvg ON ts.id = tsg.show_id
+LEFT JOIN `tv_show_genres` tsg
+ON ts.id = tsg.show_id
 WHERE tsg.show_id IS NULL
 ORDER BY ts.title, tsg.genre_id ASC;
