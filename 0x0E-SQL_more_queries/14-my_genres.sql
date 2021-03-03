d70c5cbd14c58760e29b37337a860c2da583aa9b
gchuinard <gautier.chuinard@lilo.org>
2021-03-03 16:37:59 +0000

    add task 11 - 16

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,9 +2,10 @@
 -- genres of the show Dexter.
 
 SELECT tg.name
-FROM (`tv_shows` ts JOIN tv_show_genres tsg
+FROM (`tv_shows` ts 
+	JOIN tv_show_genres tsg 
 	ON ts.id = tsg.show_id)
 JOIN `tv_genres` tg 
-ON sg.genre_id = g.id
+ON tsg.genre_id = tg.id
 WHERE ts.title = "Dexter"
 ORDER BY tg.name ASC;
