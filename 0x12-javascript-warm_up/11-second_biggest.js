704483e9461e69bd9f5ef3b118248c611369ab65
gchuinard <gautier.chuinard@lilo.org>
2021-03-22 12:37:47 +0000

    fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,16 @@
 let sec = 0;
 let pri = 0;
 
-process.argv.forEach((value) => {
-  parseInt(value);
-  if (value > pri) {
+process.argv.forEach((value, index) => {
+  value = parseInt(value);
+  pri = parseInt(pri);
+  if (pri < value) {
     sec = pri;
     pri = value;
-  } else if (value < pri && pri === sec) {
+  } else if (sec < value) {
     sec = value;
-  }
+  } else if (index === 2 && value < 0)
+    pri = value;
 });
 
 console.log(sec);
