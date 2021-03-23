e8517bd368b4bc9229185a8a1086c5a1f40f9746
gchuinard <gautier.chuinard@lilo.org>
2021-03-23 12:34:49 +0000

    fix task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,8 +2,8 @@
 
 const file = require('fs');
 const fileA = process.argv[2];
-const textA = file.readFile(fileA, 'utf8');
+const textA = file.readFileSync(fileA, 'utf8');
 const fileB = process.argv[3];
-const textB = file.readFile(fileB, 'utf8');
+const textB = file.readFileSync(fileB, 'utf8');
 const fileC = process.argv[4];
 file.writeFile(fileC, textA + textB);
