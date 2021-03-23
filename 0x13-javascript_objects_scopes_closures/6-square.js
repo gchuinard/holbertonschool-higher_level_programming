098250db9c1238631983ca1bcafe621c59c96157
gchuinard <gautier.chuinard@lilo.org>
2021-03-23 11:13:13 +0000

    fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Sqr = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Sqr {
   constructor (size) {
     super(size, size);
   }
