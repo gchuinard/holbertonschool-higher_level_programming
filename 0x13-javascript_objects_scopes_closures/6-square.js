0e3e79fcd3f5be030f4b7479944ce7f8ed0acdc2
gchuinard <gautier.chuinard@lilo.org>
2021-03-23 11:16:38 +0000

    fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,26 +2,8 @@
 const Sqr = require('./5-square');
 
 class Square extends Sqr {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c = 'X') {
-    let height;
-    let width;
-    let sqr;
-
-    height = this.size;
-    while (height > 0) {
-      width = this.size;
-      sqr = '';
-      while (width > 0) {
-        sqr += c;
-        width--;
-      }
-      console.log(sqr);
-      height--;
-    }
+    super.print(c);
   }
 }
 module.exports = Square;
