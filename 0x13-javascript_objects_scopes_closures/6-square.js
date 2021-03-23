834380928fd0da803bdd0db38b9363911343581c
gchuinard <gautier.chuinard@lilo.org>
2021-03-23 11:11:32 +0000

    fix task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,8 +6,22 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    this.print(c);
+  charPrint (c = 'X') {
+    let height;
+    let width;
+    let sqr;
+
+    height = this.size;
+    while (height > 0) {
+      width = this.size;
+      sqr = '';
+      while (width > 0) {
+        sqr += c;
+        width--;
+      }
+      console.log(sqr);
+      height--;
+    }
   }
 }
 module.exports = Square;
