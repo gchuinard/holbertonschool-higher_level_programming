037a32555601b1a2e37a6c3fe612a06cdf304874
gchuinard <gautier.chuinard@lilo.org>
2021-03-27 09:57:06 +0100

    fix task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,16 @@ Display all state in the database hbtn_0e_0_usa
 
 
 if __name == "__main__":
-    import sys
     import MySQLdb
+    import sys
 
-    argv = sys.argv
-    db = MySQLdb.connect(host=localhost,
-                         user=root,
-                         passwd=root,
-                         db=hbtn_0e_0_usa)
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute(SELECT * states ORD by id ASC)
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
