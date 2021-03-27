3e81313f563f5044131fd85662c5950ae3d9904e
gchuinard <gautier.chuinard@lilo.org>
2021-03-27 10:17:29 +0100

    fix task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY\
-                 name=%s ORDER BY id ASC", (sys.argv[4],))
+    sql = "SELECT * FROM states WHERE BINARY name=%s ORDER BY id ASC"
+    cur.execute(sql, (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
