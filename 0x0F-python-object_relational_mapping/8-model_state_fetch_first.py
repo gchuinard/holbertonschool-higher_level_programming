1fc275ac76bc90475c68edf7f38b2347d0fb7b91
gchuinard <gautier.chuinard@lilo.org>
2021-03-28 21:53:51 +0000

    fix task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,21 +12,16 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    url = {'drivername': 'mysql+mysqldb', 'host': 'localhost',
-           'username': user, 'password': password, 'database': db_name}
-
-    engine = create_engine(URL(**url), pool_pre_ping=True)
+    if len(argv) == 4:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(argv[1], argv[2], argv[3]),
+                               pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = Session(bind=engine)
-
-    try:
-        instance = session.query(State).order_by(State.id).limit(1).all()[0]
-        print("{}: {}".format(instance.id, instance.name))
-    except:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).first()
+    if query:
+        print('{}: {}'.format(query.id, query.name))
+    else:
         print("Nothing")
-
+    session.close()
