ac7e69da2f0c3dd128751edf612dc6fe854be0bd
Gautier Chuinard <45691364+gchuinard@users.noreply.github.com>
2021-04-19 15:43:21 +0200

    add task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#displays the length of the response body's
-curl -sI "$1" | grep Length | cut -d ' ' -f2
+# displays the length of the response body's
+curl -sI "$1" | grep -e Content-Length | cut -d: -f2
