90ce3f0b75f53cfc542caa18e812be0d97f44fa0
gchuinard <gautier.chuinard@lilo.org>
2021-04-20 17:30:36 +0000

    add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     url = "https://api.github.com/user"
-    print(requests.get(url, auth=(username, password)).json()['id'])
+    req = requests.get(url, auth=(username, password)).json()
+    print(req.get('id'))
